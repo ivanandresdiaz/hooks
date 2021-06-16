e0b6c0213db2e9e0947d57c64b18ea4bb2fe6edc
Andres Diaz <ivanandresdilox@gmail.com>
2021-06-16 08:03:09 -0500

    checkout container

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -2,16 +2,16 @@ import React from 'react';
 import Product from './Product';
 import '../styles/components/Products.scss';
 
-const Products = ({products}) => {
+const Products = ({ products }) => {
   return (
-    <div className="Products">
-      <div className="Products-items">
-        {products.map(product=>(
+    <div className='Products'>
+      <div className='Products-items'>
+        {products.map((product) => (
           <Product key={product.id} product={product} />
         ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Products
+export default Products;
