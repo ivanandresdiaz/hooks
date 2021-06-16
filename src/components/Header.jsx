5ed7d8fd093115f26c748f3465fbae07e8c96ef8
Andres Diaz <ivanandresdilox@gmail.com>
2021-06-16 07:51:15 -0500

    home

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../styles/components/Header.scss';
+
+const Header = () => {
+  return (
+    <div>
+      <h1 className='Header-title'>PlatziConf Merch</h1>
+      <div className='Header-checkout'>
+        Checkout
+      </div>
+    </div>
+  );
+};
+
+export default Header;
+
---
+++ src/containers/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import initialState from '../initialState';
+import Product from '../components/Products';
+
+const Home = () => {
+  return (
+    <Product products={initialState.products} />
+  );
+}
+
+export default Home;
