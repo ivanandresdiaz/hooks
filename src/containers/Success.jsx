817cf59ca11ede78f789d5e6f6a1c7a6c4a329a5
Andres Diaz <ivanandresdilox@gmail.com>
2021-06-16 22:34:33 -0500

    ready

--- src/containers/Success.jsx
+++ src/containers/Success.jsx
@@ -1,12 +1,10 @@
 import React, { useContext } from 'react';
 import AppContext from '../context/AppContext';
-import useGoogleAddress from '../hooks/useGoogleAddress';
 import '../styles/components/Success.scss';
 
 const Success = () => {
   const { state } = useContext(AppContext);
   const { buyer } = state;
-  const location = useGoogleAddress(buyer[0].address);
   return (
     <div className='Success'>
       <div className='Success-content'>
